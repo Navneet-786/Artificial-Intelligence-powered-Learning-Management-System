adb930ab498c39800104ca9bd9a4cbbcfd523cd0
Navneet <navneet@gmail.com>
2026-02-05 22:51:17 +0530

    create save user api

--- app/provider.js
+++ app/provider.js
@@ -1,33 +1,17 @@
 "use client";
-import { userTable } from "../configs/schema";
-import { db } from "../configs/db";
+
 import { useUser } from "@clerk/nextjs";
 import React, { useEffect } from "react";
-import { eq } from "drizzle-orm";
+
+import axios from "axios";
 
 const Provider = ({ children }) => {
   const { user } = useUser();
 
   const checkIsNewUser = async () => {
     //check user already exist
-    const res = await db
-      .select()
-      .from(userTable)
-      .where(eq(userTable.email, user?.primaryEmailAddress?.emailAddress));
-
-    console.log("res : ", res);
-    //if not , save to db
-    if (res?.length == 0) {
-      const userResponse = await db
-        .insert(userTable)
-        .values({
-          name: user?.fullName,
-          email: user?.primaryEmailAddress?.emailAddress,
-        })
-        .returning({ id: userTable.id });
-
-      console.log(userResponse);
-    }
+    const result = await axios.post("/api/create-user", { user: user });
+    console.log(result);
   };
   useEffect(() => {
     user && checkIsNewUser();
